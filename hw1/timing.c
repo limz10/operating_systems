2435d9dc746510506edebc39ce9fe40075e76fce
Thomas Li <mli04@tufts.edu>
2016-09-22 15:51:12 -0400

    vim

--- hw1/timing.c
+++ hw1/timing.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h> 
 #include <sys/times.h>   
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h> 
 #include <limits.h> 
 #include <time.h>
@@ -24,15 +26,16 @@ int main (int argc, char** argv) {
             printf("Error forking! %s\n", argv[1]);
          
         if (pid == 0) {
-	char * const childArgv = {argv[1], NULL};
-        execve(argv[1], childArgv, NULL);
-	printf("after");
-}
+		printf("before child\n");
+		execve(argv[1], NULL, NULL);
+		printf("after child\n");
+	}
 	
-	printf("I'm here, PID: %f", pid);
-        
-	wait();    
-        
+	else {
+		printf("here in parent, PID: %f\n", pid);
+		wait(NULL);   
+	} 
+		
     }
 
 
@@ -43,7 +46,7 @@ int main (int argc, char** argv) {
     double cuser_time = (double) (end.tms_cutime - begin.tms_cutime) / CLOCKS_PER_SEC;
     double csys_time = (double) (end.tms_cstime - begin.tms_cstime) / CLOCKS_PER_SEC;
 
-    printf("User Time: %lf\n System Time: %lf\n Child User Time: %lf\n Child System TIme: %lf\n", user_time, sys_time, cuser_time, csys_time); 
+    printf("\nUser Time: %lf\n System Time: %lf\n Child User Time: %lf\n Child System TIme: %lf\n", user_time, sys_time, cuser_time, csys_time); 
 
     return 0;
 
