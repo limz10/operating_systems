c364df66054d599d9cc0cd281da27545c09ada8a
Thomas Li <mli04@tufts.edu>
2016-09-22 20:07:52 -0400

    let go

--- hw1/timing.c
+++ hw1/timing.c
@@ -1,4 +1,4 @@
-/*
+    /*
     COMP 111 Operating Systems
     Mingzhe Li
     Fall 2016
@@ -16,43 +16,41 @@
 
 
 int main (int argc, char** argv) { 
-    struct tms begin, end;
-    pid_t pid;
-	
-	int status;
+        struct tms begin, end;
+        pid_t pid;
 
-    times(&begin);
+        int status;
 
-    for (int i = 0; i < 1; i++) {
-        pid = fork();
-        if (pid < 0) 
-            printf("Error forking! %s\n", argv[1]);
-         
-        if (pid == 0) {
-		printf("before child\n");
-		execve(argv[1], NULL, NULL);
-		exit(0);
-		printf("after child\n");
-	}
-	
-	else {
-		printf("here in parent, PID: %i\n", pid);
-		printf("Child Status: %i\n", WIFEXITED(status));
-		wait(&status);   
-	} 
-		
-    }
+        times(&begin);
+
+        for (int i = 0; i < 10; i++) {
+                pid = fork();
+                if (pid < 0) 
+                        printf("Error forking! %s\n", argv[1]);
+
+                if (pid == 0) {
+                        // printf("before child\n");
+                        execve(argv[1], NULL, NULL);
+                        exit(0);
+                        // printf("after child\n");
+                }
+                else {
+                        // printf("here in parent, PID: %i\n", pid);
+                        // printf("Child Status: %i\n", WIFEXITED(status));
+                        wait(&status);   
+                }
+        }              
 
 
-    times(&end);
+        times(&end);
 
-    double user_time = (double) (end.tms_utime - begin.tms_utime) / CLOCKS_PER_SEC;
-    double sys_time = (double) (end.tms_stime - begin.tms_stime) / CLOCKS_PER_SEC;
-    double cuser_time = (double) (end.tms_cutime - begin.tms_cutime) / CLOCKS_PER_SEC;
-    double csys_time = (double) (end.tms_cstime - begin.tms_cstime) / CLOCKS_PER_SEC;
+        double user_time = (double) (end.tms_utime - begin.tms_utime) / CLOCKS_PER_SEC;
+        double sys_time = (double) (end.tms_stime - begin.tms_stime) / CLOCKS_PER_SEC;
+        double cuser_time = (double) (end.tms_cutime - begin.tms_cutime) / CLOCKS_PER_SEC;
+        double csys_time = (double) (end.tms_cstime - begin.tms_cstime) / CLOCKS_PER_SEC;
 
-    printf("\nUser Time: %lf\n System Time: %lf\n Child User Time: %lf\n Child System TIme: %lf\n", user_time, sys_time, cuser_time, csys_time); 
+        printf("\nUser Time: %lf\nSystem Time: %lf\nChild User Time: %lf\nChild System TIme: %lf\n", 
+                user_time, sys_time, cuser_time, csys_time); 
 
-    return 0;
-
+        return 0;
 } 
