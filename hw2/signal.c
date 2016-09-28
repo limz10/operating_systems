1aaec94dc28a1d9fe0bce844dbcbca0c8ec6cf4f
limz <limz10@users.noreply.github.com>
2016-09-27 22:29:50 -0400

    hw2

---
+++ hw2/signal.c
@@ -0,0 +1,25 @@
+/*
+	COMP111 Operating Systems
+	Fall 2016	
+	HW2 Signals
+	Mingzhe Li
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+
+void alarm_handler(int signal) {
+	printf("ALARM #%i!\n", signal);
+	exit(1);
+}
+
+int main(int argc, char const *argv[])
+{
+	signal(10, alarm_handler);
+	alarm(5);
+
+	return 0;
+}
