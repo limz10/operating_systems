6eb89539fca4c720b8da566c08ebfa6cf57ebf58
limz <limz10@users.noreply.github.com>
2016-09-27 23:12:35 -0400

    signal

--- hw2/signal.c
+++ hw2/signal.c
@@ -5,17 +5,29 @@
 	Mingzhe Li
 */
 
+/*
+	This program sets up an alarm handler function, in the main()
+	the handler was setup by signal(), and alarm clock by alarm()
+
+	After 5 seconds of this program being executed, the alarm clock
+	will time out, and the program exits.
+
+	If you manually terminate the program, e.g. by pressing CTRL+C,
+	the signal message "SIGNAL #2" will show up, and the program exits.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 
 
-
 void alarm_handler(int signal) {
-	printf("ALARM #%i!\n", signal);
+	printf("SIGNAL #%i!\n", signal);
 	exit(1);
 }
+
 
 int main(int argc, char const *argv[])
 {
