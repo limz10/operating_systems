95e6c41368f303a3b0663daedf1ac508875e0917
Thomas Li <mli04@tufts.edu>
2016-10-04 15:49:33 -0400

    wordCount

--- hw3/wc.c
+++ hw3/wc.c
@@ -1,9 +1,9 @@
 /*
-	COMP111 Operating Systems
- 	Fall 2016       
- 	HW3 Multithreading
- 	Mingzhe Li
- */
+ *         COMP111 Operating Systems
+ *                 Fall 2016       
+ *                         HW3 Multithreading
+ *                                 Mingzhe Li
+ *                                 */
 
 
 #include <stdio.h>
@@ -12,12 +12,32 @@
 #include <unistd.h>
 #include <limits.h>
 #include <math.h>
+#include <pthread.h>
+#include <assert.h>
+#include <ctype.h>
+
+
+int wordCount(FILE *file) {
+	int count = 0;
+	int space = 0;
+	char c;
+	while ((c = fgetc(file)) != EOF) 
+		if (isspace(c))
+			space = 1;
+		else {
+			count += space;
+			space = 0;
+		}
+
+	return count;
+}
+
 
 
 int main(int argc, char const *argv[])
 {
-
-
-
+	FILE *f = fopen("Makefile", "r");
+	printf("Word Count: %d\n", wordCount(f));
+	
 	return 0;
 }
