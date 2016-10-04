179dbfe89e257be1354b2e06e1a2ed85748334e8
Thomas Li <mli04@tufts.edu>
2016-10-04 14:39:38 -0400

    multi threading

---
+++ hw3/wc.c
@@ -0,0 +1,23 @@
+/*
+	COMP111 Operating Systems
+ 	Fall 2016       
+ 	HW3 Multithreading
+ 	Mingzhe Li
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <limits.h>
+#include <math.h>
+
+
+int main(int argc, char const *argv[])
+{
+
+
+
+	return 0;
+}
